6520e1c4fdf6e99421e868ccbc527a62e44fc4fb
n1n3n1n3 <n1n3n1n3@protonmail.com>
2024-02-27 11:32:48 -0500

    Refactor nomenclature
    - Enlever les fichiers/dossiers inutiles
    - Changement de nomenclature de dossiers

--- Analyse_signal/Analyse_signal.py
+++ Analyse_signal/Analyse_signal.py
@@ -1,15 +1,32 @@
-import pygame
+#!/usr/bin/env python
+"""
+Script python transformant le signal MIDI de la planche de Podorythmie 5 à un signal OSC.
+Les signaux Midi sont interpretés afin de générer des signaux complexs
 
-IP = "127.0.0.1"
-PORT = "5005"
+NOTES:
+74 : PUBLIC GAUCHE GAUCHE
+76 : PUBLIC GAUCHE DROITE
+81 : PUBLIC DROITE GAUCHE
+83 : PUBLIC DROITE DROITE
+85 : ARTISTE GAUCHE GAUCHE
+86 : ARTISTE GAUCHE DROITE
+79 : ARTISTE DROITE GAUCHE
+78 : ARTISTE DROITE DROITE
 
-NUM_FSR = 8
-NUM_PIEZO = 4
-FSR_NOTES = [74, 76, 81, 83, 85, 86, 79, 78]
-PIEZO_NOTES = [100, 101, 102, 103]
 
-FSR_SENSORS = [Sensor("FSR", i, FSR_NOTES[i]) for i in range(NUM_FSR)]
-PIEZO_SENSORS = [Sensor("Piezo", i, PIEZO_NOTES[i]) for i in range(NUM_PIEZO)]
+"""
+import sys
+import os
+import time
+
+import pygame as pg
+import pygame.midi
+
+import pythonosc.udp_client
+
+from Sensor import *
+from Display import *
+
 
 def print_device_info():
     pygame.midi.init()
@@ -51,9 +68,7 @@ def input_main(device_id=None):
         
     print("using input_id :%s:" % input_id)
     midi_input = pygame.midi.Input(input_id)
-    
-    pg.display.set_mode((1, 1))
-    
+
     running = True
     while running:
         events = event_get()
@@ -61,23 +76,45 @@ def input_main(device_id=None):
             if e.type in [pg.QUIT]:
                 running = False
             if e.type in [pg.KEYDOWN]:
-                running = False
+                
+                if (e.key == pg.K_ESCAPE):
+                    running = False
+                
+                elif (e.key == pg.K_SPACE):
+                    for sensor in FSR_SENSORS:
+                        print(alternanceTriple())
+                        
             if e.type in [pygame.midi.MIDIIN]:
                 print(e)
-                
-                
+        
+#       default sensors to 0 if there is no midi signal
+        setSensorValues()
                 
         if midi_input.poll():
             midi_events = midi_input.read(10)
             midi_evs = pygame.midi.midis2events(midi_events, midi_input.device_id)
             
+            
             for m_e in midi_evs:
-                event_post(m_e)
-                
-        print('X')
-        time.sleep(0.5)
+                midiToSensor(m_e)
+                    
+                        
+    
+                    
+        screen.fill((0, 0, 0))
+        drawBoard(20, 20)
+        drawGraph(380, 20)
+        sensorToLog() 
+        showLog()
+        analysePattern()
+        pg.display.update()
+        time.sleep(0.01)
+        incrementReadIndex()
+        
         
     del midi_input
     pygame.midi.quit()
     
-input_main(1)
+input_main(3)
+
+
